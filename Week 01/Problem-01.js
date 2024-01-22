be42c607ed8efa9cc99bb23d0a187613f20a5720
Meraj Hossain <merajhossain@Merajs-MacBook-Air.local>
2024-01-22 11:31:05 +0600

    Init

---
+++ Week 01/Problem-01.js
@@ -0,0 +1,3 @@
+console.log(
+  "Recently I heard that you’ve achieved 95% marks in your exam. \nThis is brilliant! \nI wish you’ll shine in your life!\tGood luck with all the barriers(/\\) in your life."
+);
